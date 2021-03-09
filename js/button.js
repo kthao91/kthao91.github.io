bd1808d485ab9f7e54556c1305ab80dad71e7f9d
Koua Thao <kthao91@live.com>
2021-03-08 23:01:43 -0600

    Update Code
    add code to header, nav and jobs

---
+++ js/button.js
@@ -0,0 +1,16 @@
+function buttonAction(displayed) {
+    let detail = document.getElementsByClassName("detail");
+    for (var i = 0; i < detail.length; i++) {
+        let className = detail[i].className.split(" ")[1];
+        if (className != "display_none" && className == "display") {
+            console.log("true");
+            detail[i].classList.remove("display");
+            detail[i].classList.add("display_none");
+        }
+    }
+    let displayElement = document.getElementById(displayed);
+    displayElement.classList.remove("display_none");
+    displayElement.classList.add("display");
+    displayElement = document.getElementById(displayed);
+    displayElement.scrollTop = displayElement.scrollHeight;
+}
