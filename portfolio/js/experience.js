8be219acbc06dd36e02358a6811394f6199a19da
Koua Thao <kthao91@live.com>
2024-09-17 14:02:06 -0500

    added color; adding animation.

--- portfolio/js/experience.js
+++ portfolio/js/experience.js
@@ -8,9 +8,13 @@ function hide(s){
     if(s.localeCompare('edu') == 0){
         d[1].style.display = "none";
         d[0].style.display = "block";
+        d[0].classList.add('active');
+        d[1].classList.remove('active');
     }
     else if(s.localeCompare('work') == 0) {
         d[0].style.display = "none";
         d[1].style.display = "block";
+        d[1].classList.add('active');
+        d[0].classList.remove('active');
     }
 }
