3cb6d1a9002a890f38c1bd6c1bcfdebf412ac175
Koua Thao <kthao91@live.com>
2024-12-10 15:08:08 -0600

    Hero img section done
    Added an img folder
    Added BSOD
    update the matrix to do this reboot screen thing
    fix up the css a tad bit

--- javascripts/matrix.js
+++ javascripts/matrix.js
@@ -1,16 +1,16 @@
 const canvas = document.getElementById("matrix");
-
 const ctx = canvas.getContext("2d");
-
-
+let isPaused = false;
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
 const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*'+
 '()_+~`|}{[]\:;?><,./-=あかがさざただなはばぱまやらわいきぎしじちぢにひびぴみりをうくぐすずつづぬふぶぷむゆるんえけ'+
 'げせぜてでねへべぺめれおこごそぞとどのほぼぽもよろ';
 const fontSize = 10;
 const columns = canvas.width / fontSize;
+
+const img = new Image();
+img.src = "img/BSOD2.png";
 
 const drops = [];
 for (let x = 0; x < columns; x++) {
@@ -28,8 +28,7 @@ function draw() {
   for (let i = 0; i < drops.length; i++) {
     const text = chars.charAt(Math.floor(Math.random() * chars.length));
     ctx.fillText(text, i * fontSize, drops[i] * fontSize);
-    ctx.fillText(first, i * fontSize, drops[i] * fontSize);
-
+    
     if (drops[i] * fontSize > canvas.height && Math.random() > 0.975) {
       drops[i] = 0; // Reset the drop to the top
     }
@@ -37,4 +36,75 @@ function draw() {
   }
 }
 
-setInterval(draw, 33); // Adjust the speed here
+function pauseInterval() {
+  isPaused = true;
+}
+
+function startInterval() {
+  intervalId = setInterval(() => {
+    if (!isPaused) {
+
+      draw();
+
+    }
+  }, 33); 
+}
+
+function filled(){
+
+  ctx.drawImage(img,0,0,canvas.width,canvas.height);
+
+}
+
+function reset() {
+
+  let width = canvas.width;
+  let height = canvas.height;
+  let x = width/2;
+  let y = height/2;
+  let speed = 2;
+
+  function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'black';
+    ctx.fillRect(x, y, width, height);
+
+    width -= speed * 2;
+    height -= speed * 2;
+
+    //stop so it expending negativly
+    if (width <= 0 || height <= 0) {
+      x = 0;
+      y = 0;
+      console.log("loop")
+    }
+
+    else{
+      x = 0;
+      y = 0;
+    requestAnimationFrame(draw);
+    
+    }
+    console.log("end")
+
+
+  }
+  draw();
+  console.log("done")
+}
+
+function reboot(){
+  console.log("here")
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  function draw(){
+    
+  }
+  draw();
+
+}
+startInterval();
+setTimeout(pauseInterval, 15000);
+setTimeout(filled,17000);
+setTimeout(reset,18000);
+setTimeout(reboot,21000);
